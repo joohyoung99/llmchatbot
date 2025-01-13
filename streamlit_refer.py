4b8574d6278062ff200633a61de32953c39de923
joohyoung <107857210+joohyoung99@users.noreply.github.com>
2025-01-13 17:14:31 +0900

    Update streamlit_refer.py

--- streamlit_refer.py
+++ streamlit_refer.py
@@ -134,7 +134,7 @@ def get_vectorstore(text_chunks):
 
 
 def get_conversation_chain(vetorestore,openai_api_key):
-    llm = ChatOpenAI(openai_api_key=openai_api_key, model_name = 'gpt-4 mini',temperature=0)
+    llm = ChatOpenAI(openai_api_key=openai_api_key, model_name = 'gpt-3.5-turbo',temperature=0)
 
     retriever = vetorestore.as_retriever(search_type='mmr', verbose=True)
     
